8b49ebd713f72751dedae7038da2e0954781ebc2
suozg <suozg2@gmail.com>
2024-09-12 13:54:21 +0300

    Добавлены изменения в awards_v3.9.py и создан новый файл LICENSE

--- awards_v3.9.py
+++ awards_v3.9.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+# Программа распространяется под лицензией GPL2.0
+# если у вас что-то поломалось - я не виноват
+# грамар-наці йдіть за кораблем)
+
+# This file is part of awards-base.
+#
+# awards-base is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# awards-base is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with awards-base. If not, see <http://www.gnu.org/licenses/>.
+
+
 import argparse  #Process some database file
 import tkinter as tk
 from tkinter import ttk, font, filedialog
